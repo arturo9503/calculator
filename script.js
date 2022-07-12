f8d659230fff1beaba8c27238e22166deb9e7f1c
Arturo <arturo9503@gmail.com>
2022-07-12 17:52:17 -0400

    Create calculator functions

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function divide(a, b){
     return a/b;
 }
 
+function operate(selection, a, b){
+    if (selection == 'add'){
+        return add(a,b);
+    }
+    if (selection == 'subtract'){
+        return subtract(a,b);
+    }
+    if (selection == 'multiply'){
+        return multiply(a, b);
+    }
+    if (selection == 'divide'){
+        return divide(a, b);
+    }
+}
+
